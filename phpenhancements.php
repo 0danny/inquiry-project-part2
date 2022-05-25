c26341aeb3246256d78f2499f77c8cc49c115b9a
horvadi91555 <horvadi@yahoo.com.au>
2022-05-25 14:13:59 +1000

    Co-authored-by: samsharma12 <samsharma12@users.noreply.github.com>

---
+++ phpenhancements.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Node JS Enhancements">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="author" content="Adam Horvath, Sam Sharma">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/node_logo.webp">
+    <title>PHP Enhancements</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link href="styles/responsive.css" rel="stylesheet" />
+    <link rel="stylesheet" href="styles/enhancements.css">
+</head>
+<body>
+    <?php
+	    include_once "header.inc";
+    ?>
+
+    <br>
+
+    <h1 class="title">
+       PHP Enhancements
+    </h1>
+    <br>
+    <h2 class="title">
+        Enhancements made during the creation of the PHP Coding process:
+    </h2>
+
+    <h3 class="title">
+        Count check array <!-- RENAME THIS -->
+    </h3>
+
+    <section class="paragraph">
+        <p>
+            Description <!-- ENTER DESCRIPTION HERE --> <a href="markquiz.php">Markquiz</a>
+            <br><br>
+            <img src="images/countphp.png" style="width:660px;height:100px;" alt="Count $check_array variable">
+
+            <br> Website used as reference: <a href=" ">Click Here</a> <!-- ENTER WEBSITE SAM G USED -->
+        </p>
+    </section>
+
+    <br><br>
+    <h3 class="title">
+        HTML array <!-- RENAME THIS -->
+    </h3>
+
+    <section class="paragraph">
+        <p>
+            Description <!-- ENTER DESCRIPTION HERE --> <a href="quiz.php">Quiz</a>
+            <br><br>
+            <img src="images/htmlarray.png" style="width:500px;height:60px;" alt="Table Border Spacing Example">
+            <br><br>
+            <br> Website used as reference: <a href=" ">Click Here</a> <!-- ENTER WEBSITE SAM G USED -->
+          ">Click Here</a>
+        </p>
+    </section>
+
+    <?php
+	    include_once "main_footer.inc";
+    ?>
+</body>
+</html>
