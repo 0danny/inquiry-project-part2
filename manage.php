67dc95cd7f2e095e4dbaf12a88015c50e3c5e443
horvadi91555 <horvadi@yahoo.com.au>
2022-05-25 21:23:31 +1000

    CHANGES

--- manage.php
+++ manage.php
@@ -38,33 +38,51 @@
 </form>
 
 <form method="post" action="search.php" class="manage_form">
-    <p>Enter student number: <input type="text" name="student_number"></p>
-    <p><input type="submit" value="Search Student Number Attempt"></p>
+    <p>
+        <label for="student_number">Enter student number:</label> 
+        <input type="text" name="student_number"/>
+        <input type="submit" value="Search Student Number Attempt"/>
+    </p>
 </form>
 
 <form method="post" action="search.php" class="manage_form">
-    <p>Enter first name: <input type="text" name="first_name"></p>
-    <p>Enter last name: <input type="text" name="last_name"></p>
-    <p><input type="submit" value="Search Student Name Attempt"></p>
+    <p>
+        <label for="first_name">Enter first name:</label>
+        <input type="text" name="first_name"/>
+        <label for="last_name">Enter last name:</label>
+        <input type="text" name="last_name"/>
+        <input type="submit" value="Search Student Name Attempt"/>
+    </p>
 </form>
 
 <form method="post" action="search100.php" class="manage_form">
-    <p><input type="submit" value="List Students Who Got 100% On Attempt 1"></p>
+    <p>
+        <input type="submit" value="List Students Who Got 100% On Attempt 1" name='100'/>
+    </p>
 </form>
 
 <form method="post" action="search50.php" class="manage_form">
-    <p><input type="submit" value="List Students Who Got 50% Or Less On Attempt 2" ></p>
+    <p>
+        <input type="submit" value="List Students Who Got 50% Or Less On Attempt 2"/>
+    </p>
 </form>
 
 <form method="post" action="delete.php" action="delete2.php" class="manage_form">
-    <p>Enter student number: <input type="text" name="student_number"></p>
-    <p><input type="submit" value="Delete"></p>
+    <p>
+        <label for="first_name">Enter student number:</label>
+        <input type="text" name="student_number"/>
+        <input type="submit" value="Delete"/>
+    </p>
 </form>
 
 <form method="post" action="change.php" action="update.php" class="manage_form">
-    <p>Enter student number: <input type="text" name="student_number"></p>
-    <p>Enter attempt number: <input type="text" name="number_of_attempts"></p>
-    <p><input type="submit" value="Change Score Of Attempt"></p>
+    <p>
+        <label for="first_name">Enter student number:</label>
+        <input type="text" name="student_number"/>
+        <label for="number_of_attempts">Enter attempt number:</label>
+        <input type="text" name="number_of_attempts"/>
+        <input type="submit" value="Change Score Of Attempt"/>
+    </p>
 </form>
 <?php
 	    include_once "main_footer.inc";
--- update.php
+++ update.php
@@ -23,6 +23,7 @@ if ($conn) {
     if ((isset($_POST["student_number"])) && (isset($_POST["number_of_attempts"]) && (isset($_POST["score"])))){
         $student_number=trim($_POST["student_number"]);
         $number_of_attempts=trim($_POST["number_of_attempts"]);
+        $score=trim($_POST["score"]);
         $query = "UPDATE attempts SET score='$score' WHERE student_number='$student_number' AND number_of_attempts='$number_of_attempts'";
         $result = mysqli_query($conn, $query);
         if (!$result) {
@@ -31,49 +32,16 @@ if ($conn) {
             echo mysqli_errno($conn);
             }
         else {
-            if (mysqli_num_rows( $result ) !=0){
-                // Display the retrieved records
-                echo "<table border=\"1\">\n";
-                echo "<tr>\n "
-                ."<th scope=\"col\">attempt_id</th>\n "
-                ."<th scope=\"col\">date_time</th>\n "
-                ."<th scope=\"col\">first_name</th>\n "
-                ."<th scope=\"col\">last_name</th>\n "
-                ."<th scope=\"col\">student_number</th>\n "
-                ."<th scope=\"col\">number_of_attempts</th>\n "
-                ."<th scope=\"col\">score (%)</th>\n "
-            
-                ."<tr>\n ";
-            
-                // retrieve current record pointed by the result pointer
-            
-                while ($row = mysqli_fetch_assoc ($result)) {
-                    echo "<tr>\n";
-                    echo "<td>", $row["attempt_id"], "</td>\n";
-                    echo "<td>", $row["date_time"], "</td>\n";
-                    echo "<td>", $row["first_name"], "</td>\n";
-                    echo "<td>", $row["last_name"], "</td>\n";
-                    echo "<td>", $row["student_number"], "</td>\n";
-                    echo "<td>", $row["number_of_attempts"], "</td>\n";
-                    echo "<td>", $row["score"], "</td>\n";
-                    
-                    echo "</tr>\n";
-                    }
-            
-                    echo "</table>\n ";
-                    // Frees up the memory, after using the result pointer
-                    mysqli_free_result($result);
-                }
-            else{
-                echo("Something went wrong, sorry.");
-            }
-    }
+            echo "New score is $score%";
+            echo "<form method='post' action='manage.php'>";
+            echo "<p><input type='submit' value='Return to Manage Quiz Queries'></p>";
+            echo "</form>";
     }
     mysqli_close($conn);}
 else {
     echo "<p>Connection failed!</p>";
     echo mysqli_connect_error();
-}
+}}
 
 ?>
 </body>
