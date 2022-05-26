41d4b8766d16997c490e84abda21dd2f5ac36d91
horvadi91555 <horvadi@yahoo.com.au>
2022-05-26 16:05:29 +1000

    more changes

--- attempts_add.php
+++ attempts_add.php
@@ -24,7 +24,7 @@ use LDAP\Result;
         $pwd,
         $sql_db
     );
-
+if ($conn) {
     // disallows attempt if score is equal to 0
     if ($score == 0 ) {
         echo "Your score is 0. Please modify your answer.";
@@ -43,42 +43,79 @@ use LDAP\Result;
         echo "Attempts already greater than 2. <br> You cannot submit another attempt.";
     }
     else{
-    if (!$row){
-        $number_of_attempts = 1;
-    }
-    if ($row[0] == 1){
-        $number_of_attempts = 2;
-    }
-        $student_number = trim($_POST["student_number"]);
-        $first_name = trim($_POST["first_name"]);
-        $last_name = trim($_POST["last_name"]);
-        $date_time = (date("Y/m/d") . ' ' . date("h:i:sa")); // gets current time and posts it
-        $score = ($score/5)*100;
-        
-        $query = "insert into $sql_table (date_time, first_name, last_name, student_number, number_of_attempts, score) 
-        values ('$date_time', '$first_name', '$last_name', '$student_number', '$number_of_attempts', '$score')";
-
-        // execute the query - we should really check to see if the database exists first.
-            $result = mysqli_query($conn, $query);
-            // checks if the execution was successful
-            if(!$result) {
-                echo "<p class=\"wrong\">Something is wrong with ", $query, "</p>";
-            } else {
-                // display an operation successful message
-                echo "<p class=\"ok\">Successfully added New Attempt<p>";
-
-
-                
-
-
-
-
-
-
-                
-            } // if successful query operation
+        if (!$row){
+            $number_of_attempts = 1;
         }
+        if ($row[0] == 1){
+            $number_of_attempts = 2;
+        }
+            $student_number = trim($_POST["student_number"]);
+            $first_name = trim($_POST["first_name"]);
+            $last_name = trim($_POST["last_name"]);
+            $date_time = (date("Y/m/d") . ' ' . date("h:i:sa")); // gets current time and posts it
+            $score = ($score/5)*100;
+            
+            $query = "insert into $sql_table (date_time, first_name, last_name, student_number, number_of_attempts, score) 
+            values ('$date_time', '$first_name', '$last_name', '$student_number', '$number_of_attempts', '$score')";
 
+            // execute the query - we should really check to see if the database exists first.
+                $result = mysqli_query($conn, $query);
+                // checks if the execution was successful
+                
+                if(!$result) {
+                    echo "<p class=\"wrong\">Something is wrong with ", $query, "</p>";
+                } 
+                else {
+                    $query = "SELECT * FROM attempts WHERE student_number='$student_number' AND number_of_attempts='$number_of_attempts' AND score='$score'";
+                    $result = mysqli_query($conn, $query);
+                    if (!$result) {
+                        echo "<p>Something is wrong with ", $query, "</p>";
+                        echo mysqli_errno($conn);
+                        echo mysqli_error($conn);
+                    } 
+                    else{
+                        // display an operation successful message
+                        echo "<p class=\"ok\">Successfully added New Attempt<p>";
+                        // Display the retrieved records
+                        echo "<table border=\"1\">\n";
+                        echo "<tr>\n "
+                        ."<th scope=\"col\">attempt_id</th>\n "
+                        ."<th scope=\"col\">date_time</th>\n "
+                        ."<th scope=\"col\">first_name</th>\n "
+                        ."<th scope=\"col\">last_name</th>\n "
+                        ."<th scope=\"col\">student_number</th>\n "
+                        ."<th scope=\"col\">number_of_attempts</th>\n "
+                        ."<th scope=\"col\">score (%)</th>\n "
+                    
+                        ."<tr>\n ";
+                    
+                        // retrieve current record pointed by the result pointer
+                    
+                        while ($row = mysqli_fetch_assoc ($result)) {
+                            echo "<tr>\n";
+                            echo "<td>", $row["attempt_id"], "</td>\n";
+                            echo "<td>", $row["date_time"], "</td>\n";
+                            echo "<td>", $row["first_name"], "</td>\n";
+                            echo "<td>", $row["last_name"], "</td>\n";
+                            echo "<td>", $row["student_number"], "</td>\n";
+                            echo "<td>", $row["number_of_attempts"], "</td>\n";
+                            echo "<td>", $row["score"], "</td>\n";
+                            
+                            echo "</tr>\n";
+                            }
+                    
+                            echo "</table>\n ";
+                            // Frees up the memory, after using the result pointer
+                            mysqli_free_result($result);
+                            
+                            require_once('return_button.inc');
+                    }}
+                } // if successful query operation
+}
+else {
+    echo "<p>Connection failed!</p>";
+    echo mysqli_connect_error();
+}
         // close the database connection
         @mysqli_close($conn);
         // if successful database connection
