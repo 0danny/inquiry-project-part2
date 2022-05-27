8bb148b8b7a7395adf5bd675232a740e5dc9f630
horvadi91555 <horvadi@yahoo.com.au>
2022-05-27 12:17:12 +1000

    Co-authored-by: AlisterKun <AlisterKun@users.noreply.github.com>

--- manage.php
+++ manage.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8"/>
     <meta name="description" content="Management page for quiz entries." />
     <meta name="keywords" content="PHP, Mysql" />
-    <meta name="author" content="Daniel Paolone, Alister Klarica" />
+    <meta name="author" content="Daniel Paolone" />
     <title>Quiz supervisor queries</title>
 
     <link rel="icon" href="images/node_logo.webp">
@@ -30,42 +30,7 @@
         $sql_db
     );
     // Checks if connection is successful
-
-    if ($conn) {
-        $query = "SELECT * FROM users";
-        $result = mysqli_query($conn, $query);
     
-        if(!$result){
-            $query = "CREATE TABLE `users` (
-    
-                `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                `username` VARCHAR(50) NOT NULL UNIQUE,
-                `password` VARCHAR(255) NOT NULL,
-                ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1";
-    
-                $result = mysqli_query($conn, $query);
-        }
-        
-    }
-        $username = trim($_POST["username"]);
-        $password = trim($_POST["password"]);
-
-
-
-        if(isset($_POST["management_user"]))
-        {
-            if($_POST["management_user"] == $username)
-            {
-                echo '<style>.management_fieldset { display: none; }</style>';
-                include_once ("manage.inc");
-            }
-            else
-            {
-                echo "<p style=\"padding: 10px; color: red;\">Please try something else</p>";
-            }
-        }
-
-
     if(isset($_POST["management_pass"]))
     {
         if($_POST["management_pass"] == $password)
@@ -91,22 +56,12 @@
             
     <form method="post" action="manage.php" class="management_form">
         <p>
-            <label for="management_user">Enter your username: </label> 
-            <input type="username" name="management_user" placeholder="Username" max="40" size="40"/>
-        <p>
-        <p>
             <label for="management_pass">Enter the management password: </label> 
             <input type="password" name="management_pass" placeholder="Password... (Hint: Node.JS)" max="40" size="40"/>
         </p>
         <br>
 
         <input type="submit" value="Login"/>
-
-        </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
     </form>
 
 </fieldset>
